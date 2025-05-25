64e8b50d038aa27f957f49171005c7d7944dbe9d
Deraldo Nascimento <deraldosp@gmail.com>
2025-05-25 01:12:53 -0300

    feat: update command

--- app/Console/Commands/populateFakeData.php
+++ app/Console/Commands/populateFakeData.php
@@ -40,14 +40,17 @@ class populateFakeData extends Command
             $dataInicial = Carbon::createFromFormat('Y-m-d', '2025-01-15');
 
             for ($i=1; $i <= 5; $i++) {
-                Distribuicao::create([
+                $entrega = Distribuicao::create([
                     'beneficiarios_id' => $beneficiario->id,
                     'beneficios_id' => 2,
                     'unidade_beneficente_id' => 1,
                     'data_entrega' => $dataInicial,
-                    'quantidade' => 1,
+                    'quantidade' => fake()->numberBetween(1,3),
                     'created_by_id' => 1
                 ]);
+                $entrega->created_at = $dataInicial;
+                $entrega->updated_at = $dataInicial;
+                $entrega->save();
                 $dataInicial->addMonths(1);
             }
         }
@@ -67,7 +70,7 @@ class populateFakeData extends Command
             for ($i=1; $i <= 5; $i++) {
                 $tipoDoacao = fake()->numberBetween(1, 2);
                 $valor = $tipoDoacao === 2 ? fake()->randomElement([50.00, 100.00, 120.00, 150.00, 200.00]) : 0.00;
-                Doacoes::create([
+                $doacao = Doacoes::create([
                     'doadores_id' => $doador->id,
                     'tipo_id' => $tipoDoacao,
                     'unidade_id' => 1,
@@ -75,6 +78,9 @@ class populateFakeData extends Command
                     'valor' => $valor,
                     'judicial' => $valor === 120.00 ? 1 : 0
                 ]);
+                $doacao->created_at = $dataInicial;
+                $doacao->updated_at = $dataInicial;
+                $doacao->save();
                 $dataInicial->addMonths(1);
             }
         }
