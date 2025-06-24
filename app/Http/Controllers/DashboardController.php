e06cf0e51581fda58670a2b4fcdb3aee0ddd1483
Deraldo Alves do Nascimento Neto <d.do.nascimento.neto@avanade.com>
2025-06-24 15:16:53 -0300

    dashboard get initial data, add migration, fix relations

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Distribuicao;
+use App\Models\Doacoes;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboard() {
         // doações dos últimos meses
+        $startDate = Carbon::now()->startOfYear();
 
-        
+        $donations = Doacoes::selectRaw('DATE_FORMAT(created_at, "%Y-%m") as mes, COUNT(*) as total')
+            ->where('created_at', '>', $startDate->format('Y-m-d'))
+            ->groupBy('mes')
+            ->orderBy('mes')
+            ->get();
+
+
+        $donationByType = Doacoes::selectRaw('tipo_id, SUM(quantidade) as total_quantidade, SUM(valor) as total_valor')
+            ->where('created_at', '>', $startDate->format('Y-m-d'))
+            ->with('tipo')
+            ->groupBy('tipo_id')
+            ->get();
+
+        $distribuicoes = Distribuicao::selectRaw('DATE_FORMAT(data_entrega, "%m/%Y") as mes, COUNT(*) as total')
+            ->where('data_entrega', '>', $startDate->format('Y-m-d'))
+            ->groupBy('mes')
+            ->orderBy('mes')
+            ->get();
+
+        $distribuicoesPorUnidade = Distribuicao::selectRaw('unidade_beneficente_id, SUM(quantidade) as total_quantidade')->where('data_entrega', '>', $startDate->format('Y-m-d'))->with('unidade')->groupBy('unidade_beneficente_id')->get();
+
     }
 }
--- app/Models/Distribuicao.php
+++ app/Models/Distribuicao.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Distribuicao extends Model
 {
@@ -18,4 +20,10 @@ class Distribuicao extends Model
         'quantidade',
         'created_by_id'
     ];
+
+    public function unidade (): BelongsTo {
+        return $this->belongsTo(EntidadeUnidade::class, 'unidade_beneficente_id')
+            ->select(['id','nome', 'entidade_beneficentes_id'])
+            ->with('entidade');
+    }
 }
